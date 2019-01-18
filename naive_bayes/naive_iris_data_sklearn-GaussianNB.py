d267cbf092b034d05ec136feed37bef078980022
fltflt <33011440+fltflt@users.noreply.github.com>
2019-01-18 17:47:33 +0800

    Delete naive_iris_data_sklearn-GaussianNB.py

--- naive_bayes/naive_iris_data_sklearn-GaussianNB.py
+++
@@ -1,8 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import cross_val_score
-from sklearn import datasets
-iris = datasets.load_iris()
-gnb = GaussianNB()
-scores=cross_val_score(gnb, iris.data, iris.target, cv=10)
-print (scores)
-print("Accuracy:%.3f"%scores.mean())
